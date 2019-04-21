995472471ac7c96e05c291f7be1eabda17e626a8
Jezza Hehn <jezzahehn@gmail.com>
2019-04-20 23:38:48 -0400

    Initial codebase for first keyboard layout

---
+++ app/src/main/java/tech/jezza/tokiponakeyboard/FullIMS.java
@@ -0,0 +1,84 @@
+package tech.jezza.tokiponakeyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+
+
+public class FullIMS extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
+
+    @Override
+    public View onCreateInputView() {
+        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        Keyboard keyboard = new Keyboard(this, R.xml.toki_pad_full);
+        keyboardView.setKeyboard(keyboard);
+        keyboardView.setOnKeyboardActionListener(this);
+        return keyboardView;
+    }
+
+    @Override
+    public void onPress(int i) {
+
+    }
+
+    @Override
+    public void onRelease(int i) {
+
+    }
+
+    @Override
+    public void onText(CharSequence charSequence) {
+
+    }
+
+    @Override
+    public void onKey(int primaryCode, int[] keyCodes) {
+        InputConnection inputConnection = getCurrentInputConnection();
+
+        if (inputConnection != null) {
+            switch(primaryCode) {
+                case Keyboard.KEYCODE_DELETE :
+                    CharSequence selectedText = inputConnection.getSelectedText(0);
+
+                    if (TextUtils.isEmpty(selectedText)) {
+                        inputConnection.deleteSurroundingText(1, 0);
+                    } else {
+                        inputConnection.commitText("", 1);
+                    }
+
+                    break;
+                default :
+                    char code = (char) primaryCode;
+                    inputConnection.commitText(String.valueOf(code), 1);
+            }
+        }
+    }
+
+    @Override
+    public void swipeLeft() {
+//        InputConnection inputConnection = getCurrentInputConnection();
+//        if (inputConnection != null) inputConnection.commitText("i", 1);
+    }
+
+    @Override
+    public void swipeRight() {
+//        InputConnection inputConnection = getCurrentInputConnection();
+//        if (inputConnection != null) inputConnection.commitText("e", 1);
+    }
+
+    @Override
+    public void swipeDown() {
+//        InputConnection inputConnection = getCurrentInputConnection();
+//        if (inputConnection != null) inputConnection.commitText("o", 1);
+    }
+
+    @Override
+    public void swipeUp() {
+//        InputConnection inputConnection = getCurrentInputConnection();
+//        if (inputConnection != null) inputConnection.commitText("u", 1);
+    }
+}
